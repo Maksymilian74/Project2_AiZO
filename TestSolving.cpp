e1ecc99f3a094cdfb3118f1caded7222847d5e17
Maksymilian74 <273007@student.pwr.edu.pl>
2024-06-05 02:02:02 +0200

    inicjalizacja podmenu testowego

--- TestSolving.cpp
+++ TestSolving.cpp
@@ -1,1 +1,46 @@
 #include "TestSolving.h"
+#include <iostream>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+using namespace std::chrono;
+
+// Konstruktor
+TestSolving::TestSolving() {
+
+    // Inicjalizacja zmiennych do pomiaru czasu
+    start = high_resolution_clock::now();
+    stop = high_resolution_clock::now();
+    time = 0;
+}
+
+// Destruktor
+TestSolving:: ~TestSolving() {
+
+}
+
+
+// Metoda odpowiedzialna za wczytanie danych z pliku do tablicy
+void TestSolving::loadDataFromFile(string fileToOpen) {
+    ifstream file(fileToOpen); // Tworzenie strumienia do odczytu danych z pliku
+    if (!file.is_open()) {
+        cout << "-------------------------------------------\n";
+        cout << "Blad! Nie udalo sie otworzyc pliku.\n";
+        return;
+    }
+
+    file.close();
+}
+
+// Metoda odpowiedzialna za wygenerowanie grafu
+void TestSolving::generateRandomGraph(int size, int fillingMethod) {
+
+}
+
+// Metoda odpowiedzialna za wyswietlenie grafu
+void TestSolving::displayGraph() {
+
+}
+
+
--- TestSolving.h
+++ TestSolving.h
@@ -1,10 +1,33 @@
 #ifndef PROJEKT2_AIZO_TESTSOLVING_H
 #define PROJEKT2_AIZO_TESTSOLVING_H
 
+#include <chrono>
+#include <string>
+
+using namespace std;
+using namespace std::chrono;
 
 class TestSolving {
+public:
+    // Konstruktor
+    TestSolving();
 
+    // Destruktor
+    ~TestSolving();
+
+    // Metoda odpowiedzialna za wczytanie danych z pliku
+    void loadDataFromFile(string fileToOpen);
+
+    // Metoda odpowiedzialna za generowanie grafu
+    void generateRandomGraph(int size, int fillingMethod);
+
+    // Metoda odpowiedzialna za wyswietlenie grafu
+    void displayGraph();
+
+private:
+    std::chrono::high_resolution_clock::time_point start; // Zmienna przechowujaca początek pomiaru czasu
+    std::chrono::high_resolution_clock::time_point stop; // Zmienna przechowujaca koniec pomiaru czasu
+    double time; // Zmienna przechowujaca czas sortowania
 };
 
-
-#endif //PROJEKT2_AIZO_TESTSOLVING_H
+#endif
